6824e61cac992a3266ddb4a61dc639e63f6d2ce6
Kevin Silva <kevin.mss@hotmail.com>
2025-08-18 14:57:21 -0300

    feat: Implement element interaction and manipulation tests

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -44,11 +44,11 @@ describe('Formulário de Consultoria', () => {
         })
 
         cy.get('input[type="file"]')
-            .selectFile('./cypress/fixtures/1.pdf',{force: true})
+            .selectFile('./cypress/fixtures/1.pdf', { force: true })
 
         cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
             .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
-        
+
         const techs = [
             'Cypress',
             'Selenium',
@@ -57,22 +57,22 @@ describe('Formulário de Consultoria', () => {
             'Robot Framework'
         ]
 
-        techs.forEach((tech)=>{
-        cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
-            .type(tech)
-            .type('{enter}')
+        techs.forEach((tech) => {
+            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+                .type(tech)
+                .type('{enter}')
 
-        cy.contains('label','Tecnologias')
-            .parent()
-            .contains('span',tech)
-            .should('be.visible')
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible')
         })
-        
-        cy.contains('label','termos de uso')
+
+        cy.contains('label', 'termos de uso')
             .find('input')
             .check()
 
-        cy.contains('button','Enviar formulário')
+        cy.contains('button', 'Enviar formulário')
             .click()
 
         cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
