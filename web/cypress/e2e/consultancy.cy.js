27a2a716860fdc850fe587b09ea5299eea4cb2e8
Kevin Silva <kevin.mss@hotmail.com>
2025-09-13 14:21:26 -0300

    Adicionando compos obrigatórios

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -79,6 +79,26 @@ describe('Formulário de Consultoria', () => {
             .should('be.visible')
 
     })
+    it.only('Deve verificar os campos obrigatórios', () => {
+        cy.start()
+        cy.submitLoginForm('papito@webdojo.com', 'katana123')
+        cy.goTo('Formulários', 'Consultoria')
+        cy.contains('button', 'Enviar formulário')
+            .click()
+        cy.contains('p', 'Digite nome e sobrenome')
+            .should('be.visible')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+        cy.contains('p', 'Informe um email válido')
+            .should('be.visible')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+        cy.contains('p', 'Você precisa aceitar os termos de uso')
+            .should('be.visible')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+    })
 
 })
 
